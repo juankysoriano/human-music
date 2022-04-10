c2ba0ec2066e8a7695ac518de909e6c9e3722646
Juan Soriano <juanky@novoda.com>
2022-04-09 21:32:18 -0500

    Adds multiple chord progression routes

--- src/human-music/utils/data-structures/TreeNode.ts
+++ src/human-music/utils/data-structures/TreeNode.ts
@@ -1,12 +1,37 @@
+import { Chord as TonalChord, Note, Progression } from "@tonaljs/tonal";
+import { Chord } from "../../performers/audio/music-models";
+
 declare global {
     interface Array<T> {
         shuffle(): Array<T>
+    }
+
+    interface String {
+        toNode(): TreeNode<Chord>
+        toChord(): Chord
     }
 }
 
 // eslint-disable-next-line no-extend-native
 Array.prototype.shuffle = function <T>(this: T[]) {
     return this.sort(() => Math.random() - 0.5)
+}
+
+// eslint-disable-next-line no-extend-native
+String.prototype.toNode = function (this: string) {
+    return new TreeNode<Chord>(this.toChord())
+}
+
+// eslint-disable-next-line no-extend-native
+String.prototype.toChord = function (this: String) {
+    return new Chord(Progression.fromRomanNumerals("C", [this.toString()])
+        .map(note => TonalChord.get(note).notes)[0]
+        .map(note => Note.midi(`${note}0`) as number - 12)
+        .map((note, index, array) => {
+            array[index] = index > 0 && array[index - 1] >= note ? note + 12 : note
+            return array[index]
+        }), this.toString())
+
 }
 
 export class TreeNode<T> {
