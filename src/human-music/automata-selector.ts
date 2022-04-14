17ddb411d7f20b90018d7f0eaef70b7fcf1fc6a0
Juan Soriano <juanky@novoda.com>
2022-04-13 21:44:00 -0500

    Starts introducing variety of automatas

--- src/human-music/automata-selector.ts
+++ src/human-music/automata-selector.ts
@@ -3,16 +3,39 @@ import { CellularAutomata, Dimensions, Size, Type } from "./cellular-automata/in
 export class AutomataSelector {
    randomAutomata() {
       const radius = 1
-      const type = Math.random() < 0.0 ? Type.ELEMENTARY : Type.TOTALISTIC
+
+      let found = false
+      let rule = 0
+      let type = Type.ELEMENTARY
       const states = 3
-      let maxRule = 0
-      switch (type) {
-         case Type.ELEMENTARY:
-            maxRule = states ** (states ** (2 * radius + 1)) - 1
-            break
-         case Type.TOTALISTIC:
-            maxRule = states ** (states * (2 * radius + 1)) - 1
-            break
+      while (!found) {
+         type = Math.random() < 0.0 ? Type.ELEMENTARY : Type.TOTALISTIC
+         let maxRule = 0
+         switch (type) {
+            case Type.ELEMENTARY:
+               maxRule = states ** (states ** (2 * radius + 1)) - 1
+               break
+            case Type.TOTALISTIC:
+               maxRule = states ** (states * (2 * radius + 1)) - 1
+               break
+         }
+         rule = Math.floor(Math.random() * maxRule);
+         const automata = new CellularAutomata.Builder()
+            .withSize(Size.MEDIUM)
+            .withStates(states)
+            .withType(type)
+            .withDimensions(Dimensions.UNIDIMENSIONAL)
+            .withRule(rule)
+            .build()
+         const distances = new Set<number>()
+         for (let i = 0; i < 100; i++) {
+            automata.evolve()
+         }
+         for (let i = 0; i < 100; i++) {
+            distances.add(automata.leeDistance())
+            automata.evolve()
+         }
+         found = distances.size >= 5 && distances.size <= 20
       }
 
       return (
@@ -20,9 +43,8 @@ export class AutomataSelector {
             .withSize(Size.MEDIUM)
             .withStates(states)
             .withType(type)
-            //.withRandomInitialConfiguration()
             .withDimensions(Dimensions.UNIDIMENSIONAL)
-            .withRule(Math.floor(Math.random() * maxRule))
+            .withRule(rule)
             .build()
       )
    }
