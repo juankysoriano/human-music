c18a453746b26ca54add9a51ef55f4f213aab873
Juan Soriano <juanky@novoda.com>
2022-04-20 17:13:09 -0500

    Change size of automata

--- src/human-music/cellular-automata/index.ts
+++ src/human-music/cellular-automata/index.ts
@@ -16,7 +16,7 @@ export class CellularAutomata {
       private dimensions: Dimensions = Dimensions.UNIDIMENSIONAL
       private type: Type = Type.ELEMENTARY
       private states: number = 2
-      private size: number = 5
+      private size: number = 8
       private rule: number = 0
       private randomInitialConfiguration: boolean = false
 
